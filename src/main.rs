13b584b51b2078dea3fa56a8baedc02de5a9ddae
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2021-01-04 08:51:28 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use error_chain::error_chain;
+use std::io::copy;
+use std::fs::File;
+
+error_chain! {
+     foreign_links {
+         Io(std::io::Error);
+         HttpRequest(reqwest::Error);
+     }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let target = "https://pixietrixcomix.com/comics/DC_01_-034.png";
+    let response = reqwest::get(target).await?;
+
+    let mut dest = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+
+        println!("file to download: '{}'", fname);
+        let fname = tmp_dir.path().join(fname);
+        println!("will be located under: '{:?}'", fname);
+        File::create(fname)?
+    };
+    let content =  response.text().await?;
+    copy(&mut content.as_bytes(), &mut dest)?;
+    Ok(())
+}
